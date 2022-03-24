e87158165bda67367571b8217acb00a7831440f1
CrystalTri <crystaltri1234@gmail.com>
2022-03-24 10:29:04 +0700

    third commit

---
+++ src/testhello/TestHello.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testhello;
+
+/**
+ *
+ * @author ASUS GL543
+ */
+public class TestHello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+        System.out.println("Welcome to Thailand");
+        System.out.println("Have a good trip");
+    }
+    
+}
